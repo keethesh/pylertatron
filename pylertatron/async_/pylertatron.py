a9be0994afd10f6f2a954b91ce02970a0bd9daff
Keethesh <keethesh15@Gmail.com>
2022-12-01 20:42:46 +0400

    Update pylertatron.py

--- pylertatron/async_/pylertatron.py
+++ pylertatron/async_/pylertatron.py
@@ -6,6 +6,9 @@ from ..commands.command import Command
 
 class Pylertatron:
     def __init__(self, webhook_url, balance_ratio, api_key_name, client):
+        if type(client) is not AsyncClient:
+            raise TypeError("client must be of type httpx.AsyncClient, "
+                            "use create_pylertatron() to create a Pylertatron instance")
         self.webhook_url: str = webhook_url
         self.client: AsyncClient = client
         self.balance_ratio: float = balance_ratio
