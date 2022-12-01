0164dd8b47610c06f03beb4022fa7df6d3ddb0d0
Keethesh <keethesh15@Gmail.com>
2022-12-01 20:40:31 +0400

    Update __init__.py

--- pylertatron/__init__.py
+++ pylertatron/__init__.py
@@ -2,9 +2,9 @@
 Pylertatron is a Python library for sending alerts to Alertatron.
 """
 from .alert import Alert
-from .async_ import *
+import async_
 from .commands import *
-from .sync import *
+import sync
 
-__version__ = "1.0.2"
+__version__ = "1.0.0"
 __all__ = ['async_', 'sync', 'commands', 'alert']
