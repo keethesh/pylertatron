cba2a12d6fc6e817e3e1afc6ed14ae9930ebd9a2
Keethesh <keethesh15@Gmail.com>
2022-12-01 21:06:14 +0400

    Added commands.py

--- pylertatron/commands/__init__.py
+++
@@ -1,4 +0,0 @@
-from .orders import *
-from .special_commands import *
-
-__all__ = ['orders', 'special_commands'] + orders.__all__ + special_commands.__all__
--- pylertatron/commands/command.py
+++
@@ -1,4 +0,0 @@
-class Command:
-    @staticmethod
-    def format(func_name, data):
-        return f"{func_name}({', '.join(f'{k}={v}' for k, v in data.items())});"
