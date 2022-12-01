c03eb1e9c34f0ba9c9eb83e83013834f5317df52
Keethesh <keethesh15@Gmail.com>
2022-12-01 21:02:18 +0400

    Minor changes

--- pylertatron/commands/__init__.py
+++ pylertatron/commands/__init__.py
@@ -1,4 +1,4 @@
 from .orders import *
 from .special_commands import *
 
-__all__ = ['orders', 'special_commands']
+__all__ = ['orders', 'special_commands'] + orders.__all__ + special_commands.__all__
